3433e73e65c4c9b5836dce3434c6a6cb7bfa9474
lovher <84038052+youngjubest@users.noreply.github.com>
2023-05-25 22:20:23 +0900

    코드수정
    과제 제출

--- Problem1.java
+++ Problem1.java
@@ -5,25 +5,34 @@ public class week13_1 {
         Scanner input = new Scanner(System.in);
 
         // 필요한 자료형을 넣어주세요
-        ArrayList< __________ > list = new ArrayList< __________ >();
+        ArrayList<Number> list = new ArrayList<Number>();
         System.out.print("Enter five integers and five doubles: ");
 
-        /*
-            list 입력 코드
-         */
+        for (int i = 0; i < 5; i++) {
+            int num = input.nextInt();
+            list.add(num);
+        }
+
+        for (int i = 0; i < 5; i++) {
+            double num = input.nextDouble();
+            list.add(num);
+        }
 
         sort(list);
 
-        /*
-            list 출력 코드
-         */
+        System.out.println("Sorted list:");
+        for (Number num : list) {
+            System.out.print(num + " ");
+        }
     }
 
-    public static void sort( __________ ) {
+    public static void sort(ArrayList<Number> list) {
 
-         /*
-            list 정렬 코드
-         */
+         Collections.sort(list, new Comparator<Number>() {
+            public int compare(Number num1, Number num2) {
+                return Double.compare(num1.doubleValue(), num2.doubleValue());
+            }
+        });
 
     }
 }
